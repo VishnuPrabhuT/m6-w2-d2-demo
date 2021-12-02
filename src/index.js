f90ad3528ee397d782795838d82f6064cf081d79
Vishnu <vprabh@pdisoftware.com>
2021-12-01 21:04:12 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import './api/server'
+import './api/server';
 
+import { Provider } from 'react-redux';
+import store from './store';
+import { fetchTodos } from './features/todos/todosSlice';
+
+import './index.css';
+
+store.dispatch(fetchTodos());
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
